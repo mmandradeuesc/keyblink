b8bed7dccabdf2efd9029bb8d635181aa5ffc760
srspratt <hrodeberthus@gmail.com>
2025-01-18 14:01:25 -0300

    Ajustes dos outros LEDs e buzzer | Correção do teclado

--- keyblink-new.c
+++ keyblink-new.c
@@ -2,7 +2,8 @@
 #include "pico/stdlib.h"
 
 #define VECTORSIZE 4
-#define BUZZERPIN 21
+
+#define BUZZER 21
 #define GREEN 11
 #define BLUE 12
 #define RED 13
@@ -16,6 +17,7 @@ void SetOutput(uint8_t);
 
 void MapKeyboard(const uint8_t*, const uint8_t*);
 void MapRGB();
+void MapBuzzer();
 
 char TestKeys(const uint8_t*, const uint8_t*, const char(*)[4]);
 
@@ -24,12 +26,12 @@ int main() {
     const uint8_t COLUMNS[VECTORSIZE] = {5, 4, 3, 2}; //Pinos das colunas do teclado matricial
 
     const char MAPPING[4][4] = {
-// colunas ->                      linhas \/        
-    //    5    4    3    2    
+    // colunas →               linhas ↓   
+    //    5    4    3    2           
         {'1', '2', '3', 'A'}, // 9
         {'4', '5', '8', 'B'}, // 8
         {'7', '8', '9', 'C'}, // 7
-        {'*', 'B', '#', 'D'}  // 6
+        {'*', '0', '#', 'D'}  // 6
     };
 
     PicoInit(ROWS, COLUMNS);
@@ -42,17 +44,31 @@ void PicoInit(const uint8_t* ROWS, const uint8_t* COLUMNS){
     stdio_init_all();
     MapKeyboard(ROWS, COLUMNS); 
     MapRGB();
+    MapBuzzer();
 }
 
 void PicoLoop(const uint8_t* ROWS, const uint8_t* COLUMNS, const char (*MAPPING)[4]){
     while (true) {
         char key = TestKeys(ROWS, COLUMNS, MAPPING);
-        if (key == MAPPING[0][0]) // Quando 1 for pressionado
-            gpio_put(GREEN, true);
-        else
-            gpio_put(GREEN, false);
+        if (key != '$'){ // A lógica só é executada quando uma tecla for pressionada
+            if (key == '1') // Quando 1 for pressionado
+                gpio_put(GREEN, true);
+            else if (key == '2') // Quando D for pressionado
+                gpio_put(BLUE, true);
+            else if (key == '3')
+                gpio_put(RED, true);
+            else if (key == 'D')
+                gpio_put(BUZZER, true);
+            else { // Quando qualquer outra coisa for pressionada, desliga tudo
+                gpio_put(GREEN, false);
+                gpio_put(RED, false);
+                gpio_put(BLUE, false);
+                gpio_put(BUZZER, false);
+            }
+            printf("Key: %c\n", key); // Debug
+        }
         //printf("Key: %c\n", key); // Debug
-        //sleep_ms(500); // Para conseguir ver o print, kkk
+        sleep_ms(100); // Para conseguir ver o print, kkk
     }
 }
 
@@ -76,8 +92,14 @@ void MapKeyboard(const uint8_t* ROWS, const uint8_t* COLUMNS) {
 }
 
 void MapRGB(){
-    for (int i = 11; i < 13; i++) //Pinos dos leds RGB
+    for (int i = 11; i <= 13; i++) {//Pinos dos leds RGB
+        printf("pin %d \n", i);
         SetOutput(i);
+    }
+}
+
+void MapBuzzer(){
+    SetOutput(BUZZER);
 }
 
 char TestKeys(const uint8_t* ROWS, const uint8_t* COLUMNS, const char (*MAPPING)[4]) {
