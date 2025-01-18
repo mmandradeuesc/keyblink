7f740417564fd0aa771a4b4c43ea551c82efd135
srspratt <hrodeberthus@gmail.com>
2025-01-18 13:27:30 -0300

    Ativação do LED verde na tecla 1

--- keyblink-new.c
+++ keyblink-new.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#define VectorSize 4
-#define BuzzerPIN 21
+#define VECTORSIZE 4
+#define BUZZERPIN 21
+#define GREEN 11
+#define BLUE 12
+#define RED 13
 
 // Funções auxiliares
 void PicoInit(const uint8_t*, const uint8_t*);
@@ -10,12 +13,15 @@ void PicoLoop(const uint8_t*, const uint8_t*, const char (*)[4]);
 
 void SetInput(uint8_t);
 void SetOutput(uint8_t);
+
 void MapKeyboard(const uint8_t*, const uint8_t*);
+void MapRGB();
+
 char TestKeys(const uint8_t*, const uint8_t*, const char(*)[4]);
 
 int main() {
-    const uint8_t ROWS[VectorSize] = {9, 8, 7, 6}; 
-    const uint8_t COLUMNS[VectorSize] = {5, 4, 3, 2}; 
+    const uint8_t ROWS[VECTORSIZE] = {9, 8, 7, 6}; //Pinos das linhas do teclado matricial
+    const uint8_t COLUMNS[VECTORSIZE] = {5, 4, 3, 2}; //Pinos das colunas do teclado matricial
 
     const char MAPPING[4][4] = {
 // colunas ->                      linhas \/        
@@ -35,13 +41,18 @@ int main() {
 void PicoInit(const uint8_t* ROWS, const uint8_t* COLUMNS){
     stdio_init_all();
     MapKeyboard(ROWS, COLUMNS); 
+    MapRGB();
 }
 
 void PicoLoop(const uint8_t* ROWS, const uint8_t* COLUMNS, const char (*MAPPING)[4]){
     while (true) {
         char key = TestKeys(ROWS, COLUMNS, MAPPING);
-        printf("Key: %c\n", key); // debug
-        sleep_ms(500); // Para conseguir ver o print, kkk
+        if (key == MAPPING[0][0]) // Quando 1 for pressionado
+            gpio_put(GREEN, true);
+        else
+            gpio_put(GREEN, false);
+        //printf("Key: %c\n", key); // Debug
+        //sleep_ms(500); // Para conseguir ver o print, kkk
     }
 }
 
@@ -58,17 +69,22 @@ void SetOutput(uint8_t pin) {
 }
 
 void MapKeyboard(const uint8_t* ROWS, const uint8_t* COLUMNS) {
-    for (int i = 0; i < VectorSize; i++) {
+    for (int i = 0; i < VECTORSIZE; i++) {
         SetInput(ROWS[i]);  
         SetOutput(COLUMNS[i]); 
     }
 }
 
+void MapRGB(){
+    for (int i = 11; i < 13; i++) //Pinos dos leds RGB
+        SetOutput(i);
+}
+
 char TestKeys(const uint8_t* ROWS, const uint8_t* COLUMNS, const char (*MAPPING)[4]) {
-    for (int col = 0; col < VectorSize; col++) {
+    for (int col = 0; col < VECTORSIZE; col++) {
         gpio_put(COLUMNS[col], 1); 
 
-        for (int row = 0; row < VectorSize; row++) {
+        for (int row = 0; row < VECTORSIZE; row++) {
             if (gpio_get(ROWS[row])) { 
                 gpio_put(COLUMNS[col], 0); 
                 return MAPPING[row][col]; 
